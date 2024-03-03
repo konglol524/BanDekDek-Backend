def220b35e3dbc9fae264e28fc20783a18437a5b
Phuree <phureeppp@gmail.com>
2024-03-03 10:12:49 +0700

    fix deleteUserv.3

--- models/User.js
+++ models/User.js
@@ -61,14 +61,5 @@ UserSchema.methods.getSignedJwtToken = function () {
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
-//!Delete User with Cascade Booking
-UserSchema.pre("deleteOne", async function (next) {
-  try {
-    await Booking.deleteMany({ user: this._id });
-    next();
-  } catch (err) {
-    console.log(err);
-  }
-});
 
 module.exports = mongoose.model("User", UserSchema);
