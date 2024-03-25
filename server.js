6c50d85db44027caf087a65d3526864e9b000991
konglol524 <kopmul26@gmail.com>
2024-03-25 09:40:56 +0700

    added security options

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const hpp=require('hpp');
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -13,11 +18,35 @@ const rentals = require("./routes/rentals");
 const auth = require("./routes/auth");
 const bookings = require("./routes/bookings");
 
+
+var corsOptions = {
+  origin: process.env.HOST,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000, // in 10mins, api can only be accessed up to 500 times by the same user
+  max: 500
+});
+
+
 const app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(mongoSanitize());
 //add body parser
 app.use(express.json());
+//Sanitize data
+app.use(mongoSanitize());
+//security header
+app.use(helmet());
+//Prevent XSS attacks
+app.use(xss());
+//rate limiter
+app.use(limiter);
+//Prevent http param pollutions
+app.use(hpp());
+
 app.use("/api/v1/rentals", rentals);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/bookings", bookings);
